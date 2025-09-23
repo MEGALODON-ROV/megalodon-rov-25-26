ce286bd890c4a5f4ff7cbe2af44653e4c109200e
Laura Wang <lcyw2020@gmail.com>
2025-09-22 17:26:18 -0700

    finished math function
    added deadzone, rounding to ints between [1100, 1900], converting to an arduino-friendly string
    
    didn't check it yet but if no more commits then assume it's right :D

--- Training/lauraTest.py
+++ Training/lauraTest.py
@@ -3,7 +3,7 @@
 # Lx = side to side, right = positive (1900, 1100, 1100, 1900)
 # Ly = forward/back, forward = positive (1900, 1900, 1900, 1900)
 # Rx → rotation of joystick (left = positive direction, right = negative direction)
-        # ()
+        # (1100, 1900, 1100, 1900) --> if drawing the vectors, it is a diamond shape
 # A / B → buttons for up /down respectively (1 = full power, 0 = no power)
 
 frontLeftThruster = 1500
@@ -12,37 +12,40 @@ backLeftThruster = 1500
 backRightThruster = 1500
 verticalThruster = 1500
 
-def lateralScale(Lx):
-    return Lx * 400
+def scaleInput(joystickInput):
+    if joystickInput < 0.1 and joystickInput > -0.1:
+        return 0    # deadzone
+    return joystickInput * 400
 
-def forwardBackwardScale(Ly):
-    return Ly * 400
+def roundThrusterValue(thrusterValue):
+    if thrusterValue > 1900:
+        return 1900
+    elif thrusterValue < 1100:
+        return 1100
+    return int(thrusterValue)
 
-def rotationScale(Rx):
-    return Rx * 400
-
-def moveForwardBackwards(Ly):
-    fwdPower = forwardBackwardScale(Ly)
-    frontLeftThruster = fwdPower + 1500
-    frontRightThruster = fwdPower + 1500
-    backLeftThruster = fwdPower + 1500
-    backRightThruster = fwdPower + 1500
+def moveForwardBackwards(Ly, percentVert):
+    fwdPower = scaleInput(Ly) * percentVert     # scale to change how effective moving joystick is
+    frontLeftThruster = roundThrusterValue(fwdPower + 1500)
+    frontRightThruster = roundThrusterValue(fwdPower + 1500)
+    backLeftThruster = roundThrusterValue(fwdPower + 1500)
+    backRightThruster = roundThrusterValue(fwdPower + 1500)
     return (frontLeftThruster, frontRightThruster, backLeftThruster, backRightThruster)
 
-def crabbing(Lx):
-    crabPower = lateralScale(Lx)
-    frontLeftThruster = crabPower + 1500
-    frontRightThruster = -crabPower + 1500
-    backLeftThruster = -crabPower + 1500
-    backRightThruster = crabPower + 1500
+def crabbing(Lx, percentHoriz):
+    crabPower = scaleInput(Lx) * percentHoriz
+    frontLeftThruster = roundThrusterValue(crabPower + 1500)
+    frontRightThruster = roundThrusterValue(-crabPower + 1500)
+    backLeftThruster = roundThrusterValue(-crabPower + 1500)
+    backRightThruster = roundThrusterValue(crabPower + 1500)
     return (frontLeftThruster, frontRightThruster, backLeftThruster, backRightThruster)
 
 def rotate(Rx):
-    rotatePower = rotationScale(Rx)
-    frontLeftThruster = -rotatePower + 1500
-    frontRightThruster = -(frontLeftThruster)
-    backLeftThruster = -rotatePower + 1500
-    backRightThruster = -(backLeftThruster)
+    rotatePower = scaleInput(Rx)
+    frontLeftThruster = roundThrusterValue(-rotatePower + 1500)
+    frontRightThruster = roundThrusterValue(frontLeftThruster)
+    backLeftThruster = roundThrusterValue(-rotatePower + 1500)
+    backRightThruster = roundThrusterValue(backLeftThruster)
     return (frontLeftThruster, frontRightThruster, backLeftThruster, backRightThruster)
 
 def verticalMovement(A, B):
@@ -54,3 +57,16 @@ def verticalMovement(A, B):
         verticalThruster = 1500
 
     return verticalThruster
+
+def convertForArduino():
+    parts = [
+        str(frontLeftThruster),  "+",
+        str(frontRightThruster), "-",
+        str(backLeftThruster),   "=",
+        str(backRightThruster),  "/",
+        str(verticalThruster),   ")",
+        str(verticalThruster),   "(",
+        str(verticalThruster),   "!",
+        str(verticalThruster)
+    ]
+    return "".join(parts)
