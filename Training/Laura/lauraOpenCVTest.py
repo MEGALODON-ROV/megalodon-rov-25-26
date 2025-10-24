2085eb0f00875ed117906feb156244b865449d3e
Laura Wang <lcyw2020@gmail.com>
2025-10-23 17:45:09 -0700

    did resizing images and concatenating images
    there is a slight issue where if I try to grid concatenate a 2D list with different numbers of images in each row, it will not concatentate (I'll deal with that next time)

--- Training/Laura/lauraOpenCVTest.py
+++ Training/Laura/lauraOpenCVTest.py
@@ -1,6 +1,8 @@
+from email.mime import image
 import cv2
 import os
 import numpy as np
+import imutils
 
 base_path = os.path.dirname(__file__)
 
@@ -59,5 +61,77 @@ cv2.imshow("Original Image", imageToCrop)
 print(imageToCrop.shape)        # prints (rows, columns, color channels)
 croppedImage = imageToCrop[0:300, 250:400]      # trump's head!!!
 cv2.imshow("Cropped Image", croppedImage)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# RESIZING IMAGE SECTION
+# resize image width to 150 pixels, keep aspect ratio
+ratio = 150 / imageToCrop.shape[1]      # newWidth / oldWidth
+dimension = (150, int(ratio * imageToCrop.shape[0]))    # width, height aka y, x
+resizedImage = cv2.resize(imageToCrop, dimension, interpolation=cv2.INTER_AREA)
+cv2.imshow("Width Resized Image", resizedImage)
+
+# resize image to height of 50 pixels
+ratio = 50 / imageToCrop.shape[0]      # newHeight / oldHeight
+dimension = (int(ratio * imageToCrop.shape[1]), 50)
+resizedImage2 = cv2.resize(imageToCrop, dimension, interpolation=cv2.INTER_AREA)
+cv2.imshow("Height Resized Image", resizedImage2)
+
+# use imutils to resize image
+resizedImage3 = imutils.resize(imageToCrop, width = 150)
+cv2.imshow("Imutils Width Resized Image", resizedImage3)
+resizedImage4 = imutils.resize(imageToCrop, height = 50)
+cv2.imshow("Imutils Height Resized Image", resizedImage4)
+
+# half of width and height
+resizedImage5 = imutils.resize(imageToCrop, width = int(imageToCrop.shape[1] / 2))
+cv2.imshow("Half Resized Image", resizedImage5)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# notes on interpolation methods:
+# cv2.INTER_NEAREST: fast, takes neightbor pixel value and assumes intensity (bad quality)
+# cv2.INTER_LINEAR: default, calculates pixels with y=mx+b (highest quality for speed)
+# cv2.INTER_AREA: good for shrinking images, may be slower
+# cv2.INTER_CUBIC: better but slower (complicated lol, can upsample too)
+# cv2.INTER_LANCZOS4: best but slowest (not often used)
+
+# CONCATENATING IMAGES SECTION
+# concatenate images of different widths
+def vconcat_resize(img_list, interpolation = cv2.INTER_LINEAR):
+    # get the smaller width
+    w_min = min(image.shape[1] for image in img_list)
+
+    # resize images to the smaller width and store them in a new list
+    im_list_resize = [imutils.resize(image, width = w_min, inter = interpolation) for image in img_list]
+
+    return cv2.vconcat(im_list_resize)
+
+def hconcat_resize(img_list, interpolation = cv2.INTER_LINEAR):
+    # get the smaller height
+    h_min = min(image.shape[0] for image in img_list)
+
+    # resize images to the smaller height and store them in a new list
+    im_list_resize = [imutils.resize(image, height = h_min, inter = interpolation) for image in img_list]
+
+    return cv2.hconcat(im_list_resize)
+
+def concat_2D_resize(img_list_2D, interpolation = cv2.INTER_LINEAR):
+    # resize to have same height so that we can concatenate each hori line of 2D list
+    # and then concatenate each hori line so that it is ready to be concatenated vertically
+    im_list_2D_resize = [hconcat_resize(im_list, interpolation = interpolation) for im_list in img_list_2D]
+
+    return vconcat_resize(im_list_2D_resize, interpolation = interpolation)
+
+# try calling the function!
+image1 = cv2.imread(os.path.join(base_path, "trumpFunny.jpg"), cv2.IMREAD_COLOR)
+image2 = cv2.imread(os.path.join(base_path, "bidenFunny.jpg"), cv2.IMREAD_COLOR)
+
+vert_concat_image = vconcat_resize([image1, image2])
+cv2.imshow("Vertical Concatenation", vert_concat_image)
+horiz_concat_image = hconcat_resize([image1, image2])
+cv2.imshow("Horizontal Concatenation", horiz_concat_image)
+grid_concat_image = concat_2D_resize([[image1, image2], [image1, image2]])
+cv2.imshow("Grid Concatenation", grid_concat_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
