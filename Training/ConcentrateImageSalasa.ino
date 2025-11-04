f03dde2ae7b499562d16d14780830e3f23af4ca2
salasaOO <28otsukas73@stu.smuhsd.org>
2025-11-03 16:29:09 -0800

    delete

--- Training/ConcentrateImageSalasa.ino
+++
@@ -1,7 +0,0 @@
-import cv2
-
-img1 = cv2.imread('baldrat.jpg')
-img2 = cv2.imread('baldrat.jpg')
-#vertically concentrate images 
-con = cv2.vconcat([img1, img2])
-cv2.imshow('Concatenated Image', con)
