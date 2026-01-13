d82810444458127d5f7b4db48cefc668e31cff7b
Laura Wang <lcyw2020@gmail.com>
2026-01-12 18:16:14 -0800

    a template PID code for float made, many things to replace with actual sensor values

---
+++ Float/PID_template.py
@@ -0,0 +1,45 @@
+kP = 0
+kI = 0
+kD = 0
+kP_HOLD = 0
+kD_HOLD = 0
+
+def PID(target_position):
+    current_position = 0 # REPLACE with sensor returned value
+    error = current_position - target_position
+    integral = 0
+    previous_error = 0
+    derivative = 0
+    previous_output = 0  # Track previous motor output
+    ERROR_ALLOWED = 20 # cm
+    TIME_TO_HOLD = 40 # seconds
+    time_started = 0 # REPLACE with current time function
+    # printf ("Target position: %f\n", target_position)
+
+    # loop to get to target position
+    while (abs(error) > ERROR_ALLOWED):  # Continue until the error is within a small range
+        current_position = 0 # REPLACE with sensor returned value
+        error = current_position - target_position
+        integral += error
+        derivative = error - previous_error
+        output = kP * error + kI * integral + kD * derivative
+        if output < 1100:
+            output = 1100  # Minimum throttle value
+        elif output > 1900:
+            output = 1900  # Maximum throttle value
+        
+        # USE THE OUTPUT VALUE
+
+        previous_output = output  # Store last output value
+        
+        previous_error = error
+        # Add delay to prevent the loop from running too fast
+    
+    # some kind of code to hold position?
+    # either more PID-based or if robot is nice there could be some
+        # throttle value that just keeps it in place
+    while (0 - time_started < TIME_TO_HOLD):    # REPLACE 0 WITH CURRENT TIME FUNCTION
+        # maintain position
+        pass
+        
+        # USE THE OUTPUT VALUE
