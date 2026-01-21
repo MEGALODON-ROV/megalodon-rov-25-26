77191ce546ffdf6380d1c308bea8327105385d61
Laura Wang <lcyw2020@gmail.com>
2026-01-20 16:44:20 -0800

    updated PID_template to be based on whether or not we need to be maintaining the position
    function is more easily called from another main file, should extern the maintainPos variable
    maintainPos boolean would be used to begin/cancel motions

--- Float/PID_template.py
+++ Float/PID_template.py
@@ -1,10 +1,19 @@
+pidUp = [0, 0, 0]
+pidDown = [0, 0, 0]
+
+'''
 kP = 0
 kI = 0
 kD = 0
 kP_HOLD = 0
 kD_HOLD = 0
+'''
+maintainPos = True
 
 def PID(target_position):
+    kP = 0
+    kI = 0
+    kD = 0
     current_position = 0 # REPLACE with sensor returned value
     error = current_position - target_position
     integral = 0
@@ -14,7 +23,30 @@ def PID(target_position):
     TIME_TO_HOLD = 40 # seconds
     time_started = 0 # REPLACE with current time function
     # printf ("Target position: %f\n", target_position)
+    while maintainPos:
+        # when going down
+        if target_position <= current_position:
+            kP = pidDown[0]
+            kI = pidDown[1]
+            kD = pidDown[2]
+        
+        # when going up
+        else:
+            kP = pidUp[0]
+            kI = pidUp[1]
+            kD = pidUp[2]
+        
+        current_position = 0 # REPLACE with sensor returned value
+        error = current_position - target_position
+        integral += error
+        derivative = error - previous_error
+        output = kP * error + kI * integral + kD * derivative
 
+        previous_error = error
+
+        # USE THE OUTPUT VALUE
+        # DELAY TO PREVENT THE LOOP FROM RUNNING TOO FAST
+    '''
     # loop to get to target position
     while (abs(error) > ERROR_ALLOWED):  # Continue until the error is within a small range
         current_position = 0 # REPLACE with sensor returned value
@@ -31,7 +63,8 @@ def PID(target_position):
         
         previous_error = error
         # Add delay to prevent the loop from running too fast
-    
+    '''
+    '''
     # some kind of code to hold position?
     # either more PID-based or if robot is nice there could be some
         # throttle value that just keeps it in place
@@ -39,3 +72,4 @@ def PID(target_position):
         # maintain position
         # USE THE OUTPUT VALUE
         pass
+    '''
