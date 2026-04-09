2b5e317eec8a9f8660c7d3961e11133920d22e11
VihaanCodes123 <144962913+VihaanCodes123@users.noreply.github.com>
2026-04-08 18:01:24 -0700

    Update ThrusterTester.ino
    just put this here to map out new thruster name changes (to match what's labeled on the ROV).

--- Nav/ThrusterTester.ino
+++ Nav/ThrusterTester.ino
@@ -19,7 +19,7 @@ Servo VTL_T;
 Servo VBR_T;
 Servo VBL_T;
 
-Servo[] Thrusters = {FL_T, BL_T, FR_T, BR_T, VTR_T, VTL_T, VBR_T, VBL_T};
+
 
 boolean test = true;
 
@@ -28,43 +28,92 @@ void setup() {
   Serial.begin(9600);
 
   //add corresponding pin numbers like this: FL_T.attach(1)
-  FL_T.attach(4); //
-  BR_T.attach(11); //
-  BL_T.attach(8); //
-  FR_T.attach(6); //
+  LBF_T.attach(4); // yellow
+  RBB_T.attach(11); // black
+  LBB_T.attach(8); // green
+  RBF_T.attach(6); // grey
 
-  VTR_T.attach(7);
-  VTL_T.attach(9);
-  VBR_T.attach(5); //
-  VBL_T.attach(3); // 3.1 back, 2.8 forward
+  RTF_T.attach(7); // dark purple (purple not connected to gnd)
+  LTF_T.attach(9); // white
+  LTB_T.attach(5); // orange
+  RTB_T.attach(3); // blue
 
-  FL_T.writeMicroseconds(1500);
-  BL_T.writeMicroseconds(1500);
-  FR_T.writeMicroseconds(1500);
-  BR_T.writeMicroseconds(1500);    
+  FL_T.writeMicroseconds(1500); //
+  BR_T.writeMicroseconds(1500); //
+  BL_T.writeMicroseconds(1500); //
+  FR_T.writeMicroseconds(1500); //
+
   VTR_T.writeMicroseconds(1500);
   VTL_T.writeMicroseconds(1500);
-  VBR_T.writeMicroseconds(1500);
+  VBR_T.writeMicroseconds(1500); //
   VBL_T.writeMicroseconds(1500);
 
-  delay(2000);
+  
+  delay(5000);
 }
 
 
 
 void loop()
 {
-  if (test)
-  {
-    for (int i = 0; i < Thrusters.length; i++)
-    {
-      Servo thruster = Thrusters[i];
-      thruster.writeMicroseconds(1600);
-      delay(1500);
-      thruster.writeMicroseconds(1500);
-      delay(1500)
-    }
-    test = false;
-  }
+    
+  
+  // if test
+  // {
+    // Servo Thrusters[8] = {FL_T, BL_T, FR_T, BR_T, VTR_T, VTL_T, VBR_T, VBL_T};
+
+  VBL_T.writeMicroseconds(1600);
+  delay(2500);
+  VBL_T.writeMicroseconds(1500);
+  delay(500);
+  VBR_T.writeMicroseconds(1600);
+  delay(2500);
+  VBR_T.writeMicroseconds(1500);
+  delay(500);
+  BR_T.writeMicroseconds(1600);
+  delay(2500); //
+  BR_T.writeMicroseconds(1600);
+  delay(500);
+    
+
+  //   for (int i = 0; i < 8; i++)
+  //   {
+  //     Thrusters[i].writeMicroseconds(1500);
+  //   }
+    
+  //   for (int i = 0; i < 8; i++)
+  //   {
+  //     Servo thruster = Thrusters[i];
+  //     thruster.writeMicroseconds(1600);
+  //     delay(2500);
+  //     thruster.writeMicroseconds(1500);
+  //     delay(500);
+  //   }
+
+  //   test = false;
+
+  //   delay(1500);
+  // }
+  // // FL_T.writeMicroseconds(1600); //
+  // BR_T.writeMicroseconds(1600); //
+  // BL_T.writeMicroseconds(1600); //
+  // FR_T.writeMicroseconds(1600); //
+
+  // VTR_T.writeMicroseconds(1600);
+  // VTL_T.writeMicroseconds(1600);
+  // VBR_T.writeMicroseconds(1600); //
+  // VBL_T.writeMicroseconds(1600);
+
+  // delay(5000);
+
+  // FL_T.writeMicroseconds(1500); //
+  // BR_T.writeMicroseconds(1500); //
+  // BL_T.writeMicroseconds(1500); //
+  // FR_T.writeMicroseconds(1500); //
+
+  // VTR_T.writeMicroseconds(1500);
+  // VTL_T.writeMicroseconds(1500);
+  // VBR_T.writeMicroseconds(1500); //
+  // VBL_T.writeMicroseconds(1500);
   
 }
