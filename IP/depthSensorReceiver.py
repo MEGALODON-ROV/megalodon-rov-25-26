ab200125cc0ba17882715b8b7e35a2a725967636
Laura Wang <lcyw2020@gmail.com>
2026-04-20 17:40:58 -0700

    created code to send and receive/translate depth sensor data to real-world depth
    platformio works???

---
+++ IP/depthSensorReceiver.py
@@ -0,0 +1,33 @@
+import serial
+import time
+
+cereal = serial.Serial("/dev/cu.usbmodem11101", 15200, timeout=1)
+# Change the port as needed (ex. if Windows, COM3 or something like that)
+
+FACTOR = 1.004288867 # depth sent by sensor to actual depth
+
+def extract():
+    cereal.reset_input_buffer()  # Clear old data that builds up in queue
+    line = cereal.readline().decode('utf-8').strip()
+
+    complete = line.__contains__("s+") and line.__contains__("-e")
+
+    if not complete:
+        print("bad data")
+        extract()
+    else:
+
+        #print("Raw data: " + line)
+
+        try:
+            _, leftover = line.split("+")
+            data, _ = leftover.split("-")
+        except ValueError:
+            pass
+
+    return float(data) * FACTOR
+
+if __name__ == "__main__":
+    while True:
+        print(extract())
+        time.sleep(0.1)
